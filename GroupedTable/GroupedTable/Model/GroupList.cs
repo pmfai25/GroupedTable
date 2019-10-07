ced7db8fe4209741dd16e9c8850c34f215c10289
Chandu Ranwala <Chandu@CreativeSoftware.com>
2019-10-07 15:36:25 +0530

    Refactor the code

---
+++ GroupedTable/GroupedTable/Model/GroupList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace GroupedTable.Model
+{
+    public class GroupList : ObservableCollection<Car>
+    {
+        public string Header { get; set; }
+    }
+}
